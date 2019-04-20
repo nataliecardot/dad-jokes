a880ff7edcc92a6222d7d7929d4b5f98690d6f4e
Natalie-Cardot <nataliecardot@gmail.com>
2019-04-20 15:37:31 -0700

    Edit comment

--- src/retrieval-form.js
+++ src/retrieval-form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// Hitting 'Enter' with <input type="text"> focused triggers click event on the first positioned element (here, the search button). If onClick were changed to onSubmit, it causes the default submit behavior of submitting a GET request and refreshing the page (search results aren't displayed after refresh). onClick doesn't cause refresh and search results are retrieved correctly
+// Hitting 'Enter' with <input type="text"> focused triggers click event on the first positioned element (here, the search button). If onClick were changed to onSubmit, it would cause the default submit behavior of submitting a GET request and refreshing the page (search results aren't displayed after refresh). onClick doesn't cause refresh and search results are retrieved correctly
 const RetrievalForm = props => (
   <form>
     <input
