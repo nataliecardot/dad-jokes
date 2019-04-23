9530e9e646482a80e8cb004cbeceb02da6365a9c
Natalie-Cardot <nataliecardot@gmail.com>
2019-04-22 22:15:05 -0700

    Updates

---
+++ precache-manifest.2a782d36f71b70890f7cd10b3b5ffe72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "757295c42ed7d1e5c4bb",
+    "url": "/dad-jokes/static/css/main.f1d6caa5.chunk.css"
+  },
+  {
+    "revision": "757295c42ed7d1e5c4bb",
+    "url": "/dad-jokes/static/js/main.8791a433.chunk.js"
+  },
+  {
+    "revision": "03aa9e7745c6e7ee15e7",
+    "url": "/dad-jokes/static/js/runtime~main.c81b4217.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/dad-jokes/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "b6db768a7a37eec5db4756bd65dfe6e9",
+    "url": "/dad-jokes/index.html"
+  }
+];
